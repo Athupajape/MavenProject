3b666a434ebc70033a24d985e2aaf892326d0889
atharva <atharvaparanjape2@gmail.com>
2024-10-07 23:25:34 +0530

    First commit

---
+++ MavenP/src/test/java/MavenProj/MavenP/TestCase.java
@@ -0,0 +1,14 @@
+package MavenProj.MavenP;
+
+public class TestCase {
+
+	public void Print() {
+		System.out.println("Hello");
+	}
+	
+	public static void main(String[] args) {
+		TestCase obj=new TestCase();
+		obj.Print();
+	}
+	
+}
